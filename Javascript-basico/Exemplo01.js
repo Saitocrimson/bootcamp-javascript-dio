e9f91edff7efdcf2ec06e905a6e5621f2cc54413
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-07-16 22:29:43 -0200

    Create Exemplo01.js

---
+++ Javascript-basico/Exemplo01.js
@@ -0,0 +1,16 @@
+function returnEvenValues(array)
+{
+  let num=[];
+  for(var i = 0; i<array.length;)
+  {
+    
+    if  (array[i] % 2 ===0)
+    {
+      num.push(array[i]);
+    }
+  }
+  console.log(num);
+}
+
+let array =[1,2,3,4,5];
+returnEvenValues(array);
