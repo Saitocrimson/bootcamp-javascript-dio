4695b3d9cf2030484d97da6473a1455b895f6446
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-07-17 22:38:36 -0200

    Create main.js

---
+++ Javascript-basico/Contador/asset/Javascript/main.js
@@ -0,0 +1,11 @@
+var correntinha2 = document.getElementById('correntinha');
+var correntinha = 0;
+
+function increment(){
+  correntinha = correntinha +1;
+  correntinha2.innerHTML = correntinha;
+}
+function decrement() {
+  correntinha = correntinha - 1;
+  correntinha2.innerHTML = correntinha;
+}
