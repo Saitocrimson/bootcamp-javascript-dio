20e4a938c53ea8697b67417426f61ddf91522832
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-07-25 16:57:51 -0300

    Add files via upload

---
+++ Javascript-basico/light_dark/script.js
@@ -0,0 +1,35 @@
+function clique()
+{
+console.log("cliquei");
+classes();
+textos();
+}
+
+function classes()
+{
+button.classList.toggle('Dark-mode');
+h1.classList.toggle('Dark-mode');
+body.classList.toggle('Dark-mode');
+footer.classList.toggle('Dark-mode');
+}
+
+function textos()
+{
+if(button.classList.contains('Dark-mode'))
+{
+button.innerHTML = "light-mode";
+h1.innerHTML = "dark-mode" + " on";
+return;
+}
+button.innerHTML = "Dark-mode";
+h1.innerHTML = "Light-mode" + " on";
+}
+
+const button = document.getElementById('mode-seletor');
+const h1 = document.getElementById('page-title'); 
+const body= document.getElementsByTagName('body')[0];
+const footer= document.getElementsByTagName('footer')[0];
+button.addEventListener('click', clique);
+
+
+
