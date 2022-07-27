ffaee20cb7c32881805c47fefedb8c571f6576aa
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-07-27 02:11:35 -0200

    Update script.js

--- Javascript-basico/Light_Dark/asset/js/script.js
+++ Javascript-basico/Light_Dark/asset/js/script.js
@@ -7,28 +7,29 @@ textos();
 
 function classes()
 {
-button.classList.toggle('Dark-mode');
-h1.classList.toggle('Dark-mode');
-body.classList.toggle('Dark-mode');
-footer.classList.toggle('Dark-mode');
+button.classList.toggle('dark-mode');
+h1.classList.toggle('dark-mode');
+body.classList.toggle('dark-mode');
+//footer.classList.toggle('dark-mode'); por algum motivo essa parte da erro 
 }
 
 function textos()
 {
-if(button.classList.contains('Dark-mode'))
+if(body.classList.contains('dark-mode'))
 {
-button.innerHTML = "Light-mode";
-h1.innerHTML = "Dark-mode" + " on";
+button.innerHTML = 'Light mode';
+h1.innerHTML = 'Dark-mode' + ' on';
 return;
 }
-button.innerHTML = "Dark-mode";
-h1.innerHTML = "Light-mode" + " on";
+button.innerHTML = 'Dark mode';
+h1.innerHTML = 'Light mode' + ' on';
 }
 
 const button = document.getElementById('mode-seletor');
 const h1 = document.getElementById('page-title'); 
-const body= document.getElementsByTagName('body')[0];
+const body = document.getElementsByTagName('body')[0];
 const footer= document.getElementsByTagName('footer')[0];
 button.addEventListener('click',clique);
+
 
 
