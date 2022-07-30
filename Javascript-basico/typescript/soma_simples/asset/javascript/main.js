4a059ce10577dc7fcbe522e1d92a42ab403e4e03
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-07-30 19:39:19 -0300

    Create main.js

---
+++ Javascript-basico/typescript/soma_simples/asset/javascript/main.js
@@ -0,0 +1,11 @@
+let  button1 = document.getElementById('button1');
+let input1=document.getElementById('input1');
+let input2=document.getElementById('input2');
+
+function soma(num1,num2)
+{
+    if(typeof num1==='number'&& typeof num2==='number')return num1+num2;
+    else return Number(num1)+Number(num2);
+}
+
+button1.addEventListener('click', () => {console.log(soma(input1.value, input2.value))});
