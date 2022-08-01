3f4038be988b3af7806baa06348d5afe90bcf00f
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-08-01 14:54:31 -0300

    Update main.ts

--- Javascript-basico/typescript/soma_simples/asset/typescript/main.ts
+++ Javascript-basico/typescript/soma_simples/asset/typescript/main.ts
@@ -1,10 +1,16 @@
 let  button = document.getElementById('button1');
 let inputone=document.getElementById('input1') as HTMLInputElement;
 let inputt=document.getElementById('input2')as HTMLInputElement;
+let frase = "o resultado eh: ";
 
-
-function addi(num1:number ,num2:number )
+function addi(num1:number ,num2:number, printa:boolean )
 {
+    if(printa)
+    {
+        let resultado=num1+num2;
+        console.log(frase + resultado);
+
+    }
     if(typeof num1==='number'&& typeof num2==='number')return num1+num2;
     else return Number(num1)+Number(num2);
 }
@@ -12,6 +18,6 @@ function addi(num1:number ,num2:number )
 if(button)
 {
     if(inputone && inputt){
-    button.addEventListener('click', () => {console.log(addi(Number(inputone.value), Number(inputt.value)))});
+    button.addEventListener('click', () => {addi(Number(inputone.value), Number(inputt.value), true)});
     }
 }
