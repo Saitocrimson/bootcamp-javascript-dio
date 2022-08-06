f858a8d6f455f32d4924e8ac429c35ac5c8af91f
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-08-06 02:30:45 -0200

    Update main.js

--- Javascript-basico/cats/asset/javascript/main.js
+++ Javascript-basico/cats/asset/javascript/main.js
@@ -1,27 +1,24 @@
-const Base_url = 'https://thatcopy.pw/catapi/rest/';
-const button = document.getElementById('change_cat');
+const Base_urL = 'https://api.thecatapi.com/v1/images/search/';
+const button = document.getElementById('changecat');
+const getcat = async () => {
+  try {
+    const dado = await fetch(Base_urL);
+    const Json = await dado.json();
+    //  return json.webpurl;
+    return Json[0].url;
+  }
+  catch (e)
+  {
+    console.log(e.message);
 
-
-const getcat = async() =>{
-    try{
-    const dado = await fetch(Base_url);
-    const json = await dado.json();
-    return json.webpurl;
-    }
-    catch(e)
-    {
-        console.log(e.message);
-        
-    }
+  }
 };
 
 const loadimg = async () => {
-    const cat = document.getElementById('cat');
-    cat.src = await getcat();
-    
+  const cat = document.getElementById('cat');
+  cat.src = await getcat();
+
 };
 
 button.addEventListener('click', loadimg);
 loadimg();
-
-
